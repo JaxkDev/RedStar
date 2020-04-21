c6b8ae7fd33c8d592ab7f7523b114eb98e39f5b7
JaxkDev <gangnam253@gmail.com>
2020-04-21 18:05:58 +0100

    Audio Changes
    Some small things i left out...

--- Assets/Controllers/SoundController.cs
+++ Assets/Controllers/SoundController.cs
@@ -13,14 +13,14 @@ public class SoundController: MonoBehaviour {
     
     void Start() {
         WorldController.Instance.world.RegisterFurnitureCreatedCallback(this.OnFurnitureCreated);
-        WorldController.Instance.world.RegisterTileChangedCallback(this.OnTileTypeChange);
+        WorldController.Instance.world.RegisterTileChangedCallback(this.OnTileChanged);
     }
     
     void Update() {
         this.soundCooldown -= Time.deltaTime;
     }
 
-    void OnTileTypeChange(Tile tile_data) {
+    void OnTileChanged(Tile tile_data) {
         if(this.soundCooldown > 0) return;
 
         AudioClip ac = Resources.Load<AudioClip>("Sounds/Floor_OnCreated");
@@ -32,6 +32,12 @@ public class SoundController: MonoBehaviour {
         if(this.soundCooldown > 0) return;
 
         AudioClip ac = Resources.Load<AudioClip>("Sounds/"+furn.furnitureType+"_OnCreated");
+
+        if(ac == null) {
+            //No set audio, assume default.
+            ac = Resources.Load<AudioClip>("Sounds/Wall_OnCreated");
+        }
+
         AudioSource.PlayClipAtPoint(ac, Camera.main.transform.position);
         this.soundCooldown = 0.1f;
     }
