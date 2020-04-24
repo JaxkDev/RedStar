50d1e3abb726ae9096b6495cd2c7edb4835999d6
JaxkDev <gangnam253@gmail.com>
2020-04-24 20:18:33 +0100

    Start saving/loading
    Using XML Serializer with custom reader/writers.
    ff
    (Really interesting !!)

--- Assets/Scripts/Controllers/WorldController.cs
+++ Assets/Scripts/Controllers/WorldController.cs
@@ -3,12 +3,17 @@
  * Created by JaxkDev (JaxkDev@gmail.com) *
  ******************************************/
 using UnityEngine;
+using System.Xml.Serialization;
+using UnityEngine.SceneManagement;
+using System.IO;
 
 public class WorldController : MonoBehaviour {
 
 	public static WorldController Instance { get; protected set; }
 
     public World world { get; protected set; }
+
+    static bool loadWorld = false; // Stays even after reload of scene.
 
     void OnEnable() {
         if(WorldController.Instance != null) {
@@ -17,20 +22,14 @@ public class WorldController : MonoBehaviour {
 
         WorldController.Instance = this;
 
-        //Generate World:
-        this.world = new World();
-
-        //Center the Camera.
-        Camera.main.transform.position = new Vector3(this.world.Width / 2, this.world.Height / 2, Camera.main.transform.position.z);
-
-        //this.world.RandomizeTiles();
+        this.CreateEmptyWorld();
     }
 
     void Update() {
         // UPDATES THE WORLD HERE !
         // Whole world simulation...
 
-        world.Update(Time.deltaTime); // Todo possibly pause, 2x, 4x etc. (world time speed)
+        world.Update(Time.deltaTime);
     }
 
 	public Tile GetTileAtWorldPosition(Vector3 position){
@@ -38,4 +37,60 @@ public class WorldController : MonoBehaviour {
 		int y = Mathf.FloorToInt (position.y);
 		return WorldController.Instance.world.GetTileAt(x, y);
 	}
+
+
+
+
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    ///
+    ///
+    ///                             SAVING & LOADING
+    ///
+    ///
+    ////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+    public void NewWorld() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void SaveWorld() {
+        Debug.Log("Starting to save the world...");
+
+        XmlSerializer serializer = new XmlSerializer(typeof(World));
+        TextWriter writer = new StringWriter();
+        serializer.Serialize(writer, world);
+        writer.Close();
+
+        Debug.Log(writer.ToString());
+    }
+
+    public void LoadWorld() {
+        WorldController.loadWorld = true;
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void CreateEmptyWorld(){
+        //Generate World:
+        this.world = new World(100,100);
+
+        //Center the Camera.
+        Camera.main.transform.position = new Vector3(this.world.Width / 2, this.world.Height / 2, Camera.main.transform.position.z);
+    }
+
+    void CreateWorldFromSave() {
+
+        Debug.Log("create world from save");
+
+        //Generate World:
+        this.world = new World(100,100);
+
+        //Center the Camera.
+        Camera.main.transform.position = new Vector3(this.world.Width / 2, this.world.Height / 2, Camera.main.transform.position.z);
+    }
 }
--- Assets/Scripts/Models/World.cs
+++ Assets/Scripts/Models/World.cs
@@ -7,7 +7,12 @@ using System;
 using System.Diagnostics;
 using System.Collections.Generic;
 
-public class World {
+// Serializing:
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+public class World : IXmlSerializable {
 
 	Tile[,] tiles;
 
@@ -26,7 +31,7 @@ public class World {
 
     public JobQueue jobQueue;
 
-	public World(int width = 100, int height = 100){
+	public World(int width, int height){
 		this.Width = width;
 		this.Height = height;
 
@@ -54,6 +59,10 @@ public class World {
 
 
     public void Update(float deltaTime) {
+
+        // Todo possibly pause, 2x, 4x etc. (world time speed)
+        // deltaTime *= multiplier;
+
         foreach(Character c in this.characters) {
             c.Update(deltaTime);
         }
@@ -104,25 +113,11 @@ public class World {
         }
     }
 
-    /*public void RandomizeTiles(){
-		for (int x = 0; x < this.Width; x++) {
-			for (int y = 0; y < this.Height; y++) {
-				if (UnityEngine.Random.Range(0, 2) == 0) {
-					this.tiles[x, y].Type = TileType.Empty;
-				} else {
-					this.tiles[x, y].Type = TileType.Floor;
-				}
-			}
-		}
-	}*/
-
     public void InvalidateTileGraph() {
         this.tileGraph = null;
     }
 
     public void SetupPathfinding() {
-        UnityEngine.Debug.Log("SetupPathfindingExample");
-
         int l = Width / 2 - 5;
         int b = Height / 2 - 5;
 
@@ -146,7 +141,6 @@ public class World {
 
     public Tile GetTileAt(int x, int y) {
         if(x >= this.Width || x < 0 || y >= this.Height || y < 0) {
-            //UnityEngine.Debug.LogError("Tile requested at (" + x + "," + y + ") is out of range.");
             return null;
         }
         return this.tiles[x, y];
@@ -194,5 +188,35 @@ public class World {
         }
 
         return this.furniturePrototypes[furnitureType];
+    }
+
+
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    ///
+    ///
+    ///                             SAVING & LOADING
+    ///
+    ///
+    ////////////////////////////////////////////////////////////////////////////////////
+
+
+
+    public World() {
+
+    }
+
+    public XmlSchema GetSchema() {
+        return null;
+    }
+
+    public void WriteXml(XmlWriter writer) {
+        // Save info here.
+        writer.WriteAttributeString("Width", this.Width.ToString());
+        writer.WriteAttributeString("Height", this.Height.ToString());
+    }
+
+    public void ReadXml(XmlReader reader) {
+        // Read info here.
     }
 }
